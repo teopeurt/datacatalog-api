7293d13171259409436c3e35e2e4d0a6d3b13853
David James <davidcjames@gmail.com>
2010-09-23 14:51:32 -0400

    Version 0.4.15

--- resources/_root.rb
+++ resources/_root.rb
@@ -5,7 +5,7 @@ module DataCatalog
 
     META = {
       "name"    => "National Data Catalog API",
-      "version" => "0.4.14",
+      "version" => "0.4.15",
       "resource_directory" => {
         "href" => "/resources"
       },
