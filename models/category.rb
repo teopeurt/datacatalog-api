f82583e28332a52527fb3d4e1b12f29ef30e0f8c
David James <davidcjames@gmail.com>
2010-09-23 14:44:12 -0400

    Add slug to category model

--- models/category.rb
+++ models/category.rb
@@ -4,10 +4,16 @@ class Category
 
   # == Attributes
 
-  key :name, String
+  key :name,        String
+  key :slug,        String
   timestamps!
 
   # == Indices
+
+  # == Indices
+
+  ensure_index :name
+  ensure_index :slug
 
   # == Associations
 
@@ -20,6 +26,25 @@ class Category
   # == Validations
 
   validates_presence_of :name
+  validates_uniqueness_of :slug
+  validates_format_of :slug,
+    :with      => /\A[a-zA-z0-9\-]+\z/,
+    :message   => "can only contain alphanumeric characters and dashes",
+    :allow_nil => true
+
+  before_validation :handle_blank_slug
+  def handle_blank_slug
+    self.slug = nil if self.slug.blank?
+  end
+
+  # == Callbacks
+
+  before_create :generate_slug
+  def generate_slug
+    return unless slug.blank?
+    return if name.blank?
+    self.slug = Slug.make(name, self)
+  end
 
   # == Class Methods
 
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -47,6 +47,84 @@ class CategoryUnitTest < ModelTestCase
       use "invalid category"
       use "category.name can't be empty"
     end
+
+    context "slug" do
+      context "new" do
+        before do
+          @category = Category.new(@valid_params)
+        end
+
+        after do
+          @category.destroy
+        end
+
+        test "on validation, not set" do
+          assert_equal true, @category.valid?
+          assert_equal nil, @category.slug
+        end
+
+        test "on save, set based on title" do
+          assert_equal true, @category.save
+          assert_equal "science-technology", @category.slug
+        end
+      end
+
+      context "create" do
+        before do
+          @category = Category.create(@valid_params)
+        end
+
+        after do
+          @category.destroy
+        end
+
+        test "set based on title" do
+          assert_equal "science-technology", @category.slug
+        end
+      end
+
+      context "update" do
+        before do
+          @category = Category.new(@valid_params)
+        end
+
+        after do
+          @category.destroy
+        end
+
+        test "unchanged after multiple saves" do
+          @category.save
+          assert_equal "science-technology", @category.slug
+          @category.save
+          assert_equal "science-technology", @category.slug
+        end
+
+        test "disallow duplicate slugs" do
+          @category.slug = "in-use"
+          @category.save
+          @new_category = Category.new(@valid_params)
+          @new_category.slug = "in-use"
+          assert_equal false, @new_category.valid?
+          expected = { :slug => ["has already been taken"] }
+          assert_equal expected, @new_category.errors.errors
+        end
+
+        test "prevent duplicate slugs" do
+          params = @valid_params.merge(:name => "Common")
+          @category = Category.create(params)
+
+          category_2 = Category.create!(params)
+          assert_equal "common-2", category_2.slug
+
+          category_3 = Category.create!(params)
+          assert_equal "common-3", category_3.slug
+
+          category_2.destroy
+          category_3.destroy
+        end
+      end
+    end
+
   end
 
 end
